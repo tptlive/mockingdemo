91ae1dc4d4749b362d986d54da03bcf3710d507f
Dmitri Maksimov <dmitri.maksimov@tptlive.ee>
2018-04-12 17:01:33 +0300

    Initial commit

---
+++ src/main/java/ee/tptlive/testid/mockingdemo/rest/JAXRSConfiguration.java
@@ -0,0 +1,9 @@
+package ee.tptlive.testid.mockingdemo.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class JAXRSConfiguration extends Application {
+
+}
